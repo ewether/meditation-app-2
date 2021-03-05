dfafbebd9061c25076d78ab411dd1637ea3198cc
Elise Wetherholt <ewether@gmail.com>
2021-03-05 08:22:21 -0500

    audio files, new components, added basic jsx

---
+++ src/components/Countdown.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+function Countdown() {
+    return (
+        <div className="countdown-timer">
+            <div className="time-left">
+                10:00
+            </div>
+        </div>
+    )
+}
+
+export default Countdown;
---
+++ src/components/UserOptions.js
@@ -0,0 +1,56 @@
+// import { render } from '@testing-library/react';
+import React from 'react';
+import '../App.css';
+
+function UserOptions() {
+    return (
+        <div className="options-wrapper">
+        <div className="time-options">
+            <ul>
+                <li className="time-opt" id="time-opt-5">
+                    5min
+                </li>
+                <li className="time-opt" id="time-opt-15">
+                    15min
+                </li>
+                <li className="time-opt" id="time-opt-30">
+                    30min
+                </li>
+            </ul>
+        </div>
+        <div className="music-options">
+            <ul>
+                <li className="music-opt" id="music-opt-1">
+                    1
+                    <audio id="audio-1" preload="auto">
+                        {/* <source src="./audio/calm1.mp3"> </source> */}
+                    </audio>
+                </li>
+                <li className="music-opt" id="music-opt-2">
+                    2
+                    <audio id="audio-2" preload="auto">
+                        {/* <source src="./audio/calm2.mp3"> </source> */}
+                    </audio>
+                </li>
+                <li className="music-opt" id="music-opt-3">
+                    3
+                    <audio id="audio-3" preload="auto">
+                        {/* <source src="./audio/calm3.mp3"> </source> */}
+                    </audio>
+                </li>
+                <li className="music-opt" id="music-opt-4">
+                    4
+                    <audio id="audio-4" preload="auto">
+                        {/* <source src="./audio/calm4.mp3"> </source> */}
+                    </audio>
+                </li>
+            </ul>
+        </div>
+        <button className="begin-button">
+            Begin
+        </button>
+    </div>
+    );
+};
+
+export default UserOptions;
