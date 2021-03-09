0159094af43b05d9cf8b7ab3e3602bf961776940
jqn <jqngl@icloud.com>
2021-03-09 08:09:50 +0100

    Refactored loader title and added hide effect in main container

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,19 +1,7 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import PropTypes from "prop-types";
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 const Loader = () => {
-  return <View style={styles.container} />;
+  return <h2 className="on-load-text">Let's Relax</h2>;
 };
-
-Loader.defaultProps = {};
-
-Loader.propTypes = {};
 
 export default Loader;
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Loader from "../components/Loader";
+import UserOptions from "../components/UserOptions";
 
 function Main() {
+  const [loading, setLoading] = useState(true);
+
+  // Use an empty dependency array
+  // to run useEffect once on mount
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // hide loader text after two seconds
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="main">
+      <UserOptions />
     </div>
   );
 }
