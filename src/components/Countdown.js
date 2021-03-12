425a428b22c9a2d8116337a861e50198d2e054e5
jqn <jqngl@icloud.com>
2021-03-12 16:41:48 +0100

    Fixed merge conflics in App.css and added count prop to countdown

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -4,7 +4,7 @@ function padTime(time) {
   return time.toString().padStart(2, "0");
 }
 
-function Countdown() {
+function Countdown({ count }) {
   const [timeLeft, setTimeLeft] = useState(25 * 60);
   const [isRunning, setIsRunning] = useState(false);
   const intervalRef = useRef(null);
